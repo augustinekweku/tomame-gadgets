0146bd097158682ae8cd9611fe10819461d456ea
augustine nani <mac@macs-MacBook-Pro.local>
2024-12-02 09:50:57 +0000

    add breadcrumb to product details page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -3,6 +3,8 @@ import { ChatBubbleIcon } from "@radix-ui/react-icons";
 import React from "react";
 import { PortableText } from "@portabletext/react";
 import { TypedObject } from "sanity";
+import Link from "next/link";
+import { truncateText } from "@/utils";
 
 type ProductDeatilsProps = {
   product: IProduct;
@@ -17,6 +19,55 @@ const ProductDetails = ({ product }: ProductDeatilsProps) => {
     <div>
       <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
         <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+          <div className="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8 ">
+            <div>
+              <nav className="flex" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                  <li className="inline-flex items-center">
+                    <Link
+                      href="/"
+                      className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
+                    >
+                      <svg
+                        className="me-2.5 h-3 w-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                      </svg>
+                      All products
+                    </Link>
+                  </li>
+                  <li aria-current="page">
+                    <div className="flex items-center">
+                      <svg
+                        className="h-5 w-5 text-gray-400 rtl:rotate-180"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m9 5 7 7-7 7"
+                        />
+                      </svg>
+                      <span className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">
+                        {truncateText(product.title, 20)}
+                      </span>
+                    </div>
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
           <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
             <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
               <img
