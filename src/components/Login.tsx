9c98f210e81681c52c169a57e535b639be5596e8
Augustine Nani <augustine.nani@hubtel.com>
2025-03-12 22:48:00 +0000

    login page copy update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,8 +16,8 @@ export function Login() {
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+            <p className="text-muted">
+              Welcome! Login to your account to continue
             </p>
           </div>
           <div className="grid gap-4">
