47d0e77071b564d7a19cbf62221b0da8479cdf4f
Augustine Nani <augustine.nani@hubtel.com>
2025-03-13 07:26:44 +0000

    login page update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
@@ -57,12 +56,6 @@ export function Login() {
               </svg>
               Login with Google
             </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
           </div>
         </div>
       </div>
