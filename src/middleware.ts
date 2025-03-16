25edce9d12b5272b0b05ae08ffa6593d7b4904c5
Augustine Nani <augustine.nani@hubtel.com>
2025-03-16 07:42:02 +0000

    troubleshoot middleware nextauth issue: add correct nextauth url and update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,31 @@
 import { NextConfig } from "next";
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
+import { authorizedUsers } from "./lib/authOptions";
 
 export default withAuth(
   function middleware(req) {
     const session = req.nextauth?.token;
     const pathname = req.nextUrl.pathname;
     const hasSesssion = session;
-
+    const userEmail = session?.email;
+    const isUserAuthrized = authorizedUsers.includes(userEmail as string);
     const isInAuthRoute = pathname.includes("/auth");
     const isProtectedRoute = pathname.includes("/admin");
 
-    if (isInAuthRoute && hasSesssion) {
+    if (isInAuthRoute && hasSesssion && isUserAuthrized) {
       return NextResponse.redirect(new URL("/admin", req.url));
     }
-
-    console.log("Session", session);
-    console.log("Pathname", pathname);
-    console.log("Has Session", hasSesssion);
-    console.log("Is in Auth Route", isInAuthRoute);
-    console.log("Is Protected Route", isProtectedRoute);
-    console.log("req.url", req.url);
 
     if (isProtectedRoute && !hasSesssion) {
       return NextResponse.redirect(new URL("/auth/login", req.url));
     }
+
+    if (isProtectedRoute && hasSesssion && !isUserAuthrized) {
+      return NextResponse.redirect(new URL("/auth/access-denied", req.url));
+    }
+
+    return NextResponse.next();
   },
   {
     callbacks: {
